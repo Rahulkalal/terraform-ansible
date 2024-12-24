094022e52521997c0b044d94f96bdc3a9cbd01b8
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2024-12-24 10:36:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-2"  # Change to your desired region
+  region = "us-west-1"  # Change to your desired region
 }
 
 # Create a security group that allows inbound traffic on port 80 (HTTP)
